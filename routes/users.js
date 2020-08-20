d90771ffa797672605ca8ce245d50249c4a5380a
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2020-08-21 01:00:15 +0800

    test

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/:user', function (req, res, next) {
+  res.json(req.params);
 });
 
 module.exports = router;
